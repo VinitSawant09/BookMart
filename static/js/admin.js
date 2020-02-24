5986467d833de2a0f7548b7853e15fa3b50b5ad7
VinitSawant09 <55744016+VinitSawant09@users.noreply.github.com>
2020-02-24 13:49:11 +0000

    addBooksForm
    validation

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,7 +1,63 @@
-function addBooks()
+function addBooksRedirect ()
 {
 document.getElementById("changePassword").style.display = 'none';
 document.getElementById("addBooks").style.display = 'block';
+}
 
+function addBookValid()
+{
+ document.getElementById("errorDivAddBooks").innerHTML = '';
+var title = document.getElementById("booktitle").value;
+var author =  document.getElementById("author").value;
+var desc =  document.getElementById("desc").value;
+var year = document.getElementById("year").value;
+var cost = document.getElementById("cost").value;
+  var image =  document.getElementById("img").value;
+if(title=='' || author ==''|| desc=='' || year==''|| cost =='' || image =='' )
+{
+
+  document.getElementById("errorDivAddBooks").innerHTML = 'Fields cannot be empty';
+  return false;
+
+}
+else if( ValidateImg()==false )
+{
+document.getElementById("errorDivAddBooks").innerHTML = 'Uploaded file is not an image.!';
+}
+return true;
+
+}
+
+
+function ValidateImg() {
+
+    var _validFileExtensions = ["jpg", "jpeg", "bmp", "gif", "png"];
+    var img =  document.getElementById("img").value;
+    img = img.split('.').pop();
+
+    for (var i=0 ;i< _validFileExtensions.length ; i++ )
+    {
+     if (img == _validFileExtensions[i])
+     {
+      return true ;
+     }
+    }
+
+    return false;
+}
+
+
+
+
+function addBook()
+{
+
+   if(addBookValid())
+   {
+     return true;
+
+   }
+
+return false;
 
 }
