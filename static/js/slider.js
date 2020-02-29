d5c742de62f6690f4944066b03e151651aaed03f
VinitSawant09 <55744016+VinitSawant09@users.noreply.github.com>
2020-02-29 00:05:20 +0000

    Slider on click

--- static/js/slider.js
+++ static/js/slider.js
@@ -23,8 +23,8 @@ $.ajax(
         processData: false,
         success: function(response){
         entireData=response
-        console.log(response[0][1])
-        console.log(entireData)
+       // console.log(response[0][1])
+       // console.log(entireData)
         var $sectionId = $("#search-car")
         count=entireData.length;
         var authors=[];
@@ -91,4 +91,260 @@ $.ajax(
 
         }
        });
+
+     //search  book functionality starts
+	       $("#search").click(function()
+	            {
+	    	         //console.log(entireData)
+                     finalData=[];
+	    	         //clearing the details
+     	             document.getElementById("search-car-details").style.display = "";
+     	           //  document.getElementById("selected-car-details").style.display = "none";
+
+     	           //  $('#expandedImg').removeAttr('src');
+
+	            	 var authorState = $("#authorsL").children(":selected").attr("value");
+	   	             var yearState = $("#yearsL").children(":selected").attr("value");
+	   	             var maxState = $("#maxPrice").children(":selected").attr("value");
+	   	             var minState = $("#minPrice").children(":selected").attr("value");
+
+                     /*
+	   	             //clearing the car details
+	   	             document.getElementById("open-button").style.display = "";
+	   	 	         document.getElementById("carName").innerHTML="";
+	   	 	         document.getElementById("carModel").innerHTML="";
+	   	 	         document.getElementById("carType").innerHTML="";
+	   	 	         document.getElementById("carPrice").innerHTML="";
+	   	 	         document.getElementById("carYear").innerHTML="";*/
+	   	             document.getElementById("noresult").style.display ="none";
+
+	            	 var $sectionId = $("#search-car")
+
+	            	 const myNode = document.getElementById("search-car");
+	            	 myNode.innerHTML = '';
+	            	 /*
+	            	 const image1 = document.getElementById("image1");
+	            	 image1.innerHTML = '';
+	            	 const image2 = document.getElementById("image2");
+	            	 image2.innerHTML = '';
+	            	 const image3 = document.getElementById("image3");
+	            	 image3.innerHTML = '';
+	            	 const expandedImg =document.getElementById("expandedImg");
+	            	 expandedImg.innerHTML ='';
+	            	  */
+	            	 var element = document.getElementById("search-car");
+
+	            	 //removing slickslider
+	            	 element.classList.remove("slick-initialized");
+	            	 element.classList.remove("slick-slider");
+
+
+	            	//filter conditions
+	           		 if(authorState == "All Authors" && yearState=="All Years" )
+	            		 {
+	           			    if (minState ==0 && maxState== 0)
+	           			    	{
+	           			    		finalData=entireData;
+	           			    	}
+	           			    else
+	           			    	{
+
+
+	           			    	     j=0;
+	           			    		 for (var i = 0; i < entireData.length; i++)
+    	            		 			{
+
+	           			    			 if (maxState > entireData[i][4] && minState < entireData[i][4])
+	           			    				 {
+
+	           			    					finalData[j]=entireData[i];
+    	            							j++;
+	           			    				 }
+    	            		 			}
+	           			    	}
+                            console.log(finalData.length);
+	           			 	for(var i=0;i<finalData.length;i++ )
+	            	 			{
+	            	 		        var id= "image_"+finalData[i][0];
+
+	        	 					$sectionId.append($("<div>").append($("<img>").attr('src',"data:image/jpg;base64, "+ finalData[i][6])));
+
+	            	 			}
+
+
+	           			 	 var windowWidth = $(window).width();
+
+                           // alert(windowWidth);
+
+                             if( windowWidth >= 320 &&  windowWidth<=500)
+                            	 {
+                            	 $(".regular").slick({
+
+    	 	            	   		 dots: false,
+    	 	            	  		 infinite: true,
+    	 	            	    	 slidesToShow: 1,
+    	 	            	  	     slidesToScroll: 1,
+
+
+    	 	            	 		 });
+
+                            	 }
+                             else if( windowWidth >= 501 &&  windowWidth<=767)
+                        	 {
+                        	 $(".regular").slick({
+
+	 	            	   		 dots: false,
+	 	            	  		 infinite: true,
+	 	            	    	 slidesToShow: 2,
+	 	            	  	     slidesToScroll: 1,
+
+
+	 	            	 		 });
+
+                        	 }
+                             else if( windowWidth >= 768 &&  windowWidth<=900)
+                        	 {
+                        	 $(".regular").slick({
+
+	 	            	   		 dots: false,
+	 	            	  		 infinite: true,
+	 	            	    	 slidesToShow: 3,
+	 	            	  	     slidesToScroll: 1,
+
+
+	 	            	 		 });
+
+                        	 }
+
+                             else{
+
+                            	 $(".regular").slick({
+
+    	 	            	   		 dots: false,
+    	 	            	  		 infinite: true,
+    	 	            	    	 slidesToShow: 4,
+    	 	            	  	     slidesToScroll: 4
+    	 	            	 		 });
+                             }
+
+
+	            		 }
+	            	 else
+	            		 {
+
+	            			 if (maxState ==0)
+	            				{
+	            					maxState=100000;
+	            				}
+
+	            			 j=0;
+	    	           		  for (var i = 0; i < entireData.length; i++)
+	    	            		 {
+	    	            		   if(authorState == "All Authors")
+	    	            			 {
+	    	            				if(yearState!="All Years" && yearState ==entireData[i][5] && (maxState > entireData[i][4] && minState < entireData[i][4]) )
+
+	    	            					{
+	    	            						finalData[j]=entireData[i];
+	    	            						j++;
+
+	    	            					}
+	    	            				else if (yearState=="All Years" && (maxState > entireData[i][4] && minState < entireData[i][4]))
+
+	    	            					{
+
+	    	            						finalData[j]=entireData[i];
+         										j++;
+
+	    	            					}
+	    	            			 }
+	    	            	  		else
+	    	             	   	 		{
+
+	    	            	  			 if(authorState == entireData[i][2] && yearState!="All Years" && yearState==entireData[i][5] &&  (maxState > entireData[i][4] && minState < entireData[i][4]) )
+
+	            							{
+	            								finalData[j]=entireData[i];
+	            								j++;
+	            							}
+	            					 	 else if (authorState == entireData[i][2] && yearState=="All Years" &&  (maxState > entireData[i][4] && minState < entireData[i][4]))
+
+	            							{
+
+	            								finalData[j]=entireData[i];
+												j++;
+
+	            							}
+                                         //  alert(finalData.length);
+	    	               				 }
+
+
+	    	                 }
+                        console.log(finalData)
+	    	           	for(var i=0;i<finalData.length;i++)
+        	 				{
+
+        	 					var id= "image_"+finalData[i][0];
+
+	        	 					$sectionId.append($("<div>").append($("<img>").attr('src',"data:image/jpg;base64, "+ finalData[i][6])));
+
+
+        	 				}
+
+
+
+            		  	 $(".regular").slick({
+
+	            	   				 dots: false,
+	            	  		 		 infinite: true,
+	            	    			 slidesToShow: 4,
+	            	  	  			 slidesToScroll: 4,
+	            	  	  			 focusOnSelect:true,
+	            	  	  			  touchMove: false,
+	            	 			 });
+
+
+
+	    	           	if (finalData.length==0)
+	    	           		{
+	    	           		alert("0");
+	    	           		document.getElementById("noresult").style.display ="";
+	    	           		document.getElementById("noresult").innerHTML="No Books available...sorry";
+
+	    	           		}
+	            		 }
+                    //action on clicking any book image on slick slider
+
+                    $('.slick-slide').on('click', function(ev)
+	           			{
+	           			       console.log("clicked.!!");
+	           		           index=$(ev.currentTarget).data('slick-index')
+	           		           console.log(index);
+	           		           console.log(finalData[index]);
+
+
+
+
+
+
+	           		     });
+
+
+
+	           		//scroll animation using jquery
+	           		/*
+	           		$('html, body').animate({
+	        	        scrollTop: $("#search-car-details").offset().top
+	        	    }, 500);
+                    */
+
+	             });
+
+
+
+
+
+
+
+
 }
